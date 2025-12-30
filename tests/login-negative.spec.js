76d8dfa9293c081e8ade1cd6a291ce10e341b9e4
yusrilazharp <yusrilazharpermana@gmail.com>
2025-12-30 07:27:11 +0700

    Refactor locators using role and label selectors

--- tests/login-negative.spec.js
+++ tests/login-negative.spec.js
@@ -3,8 +3,9 @@ import { test, expect } from '@playwright/test';
 test('Login fails when email is empty', async ({ page }) => {
   await page.goto('https://the-internet.herokuapp.com/login');
 
-  await page.fill('#password', 'SuperSecretPassword!');
-  await page.click('button[type="submit"]');
+  await page.getByLabel('Password').fill('SuperSecretPassword!');
+  await page.getByRole('button', { name: 'Login' }).click();
+
 
   const errorMessage = page.locator('#flash');
   await expect(errorMessage).toContainText('Your username is invalid');
